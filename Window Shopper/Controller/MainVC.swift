d32b2c273a33d7643a06e6c451123c2c8425ecc2
Sain-R Edwards Jr <SKE@Sain-Rs-iMac-5.local>
2018-03-01 19:00:12 -0800

    Added IBDesignable to top of class so the IB will update automatically, also added prepareForInterfaceBuilder function, and a custom function so it would work. Added a custom calculate button to the CurrencyTxtField class.

--- Window Shopper/Controller/MainVC.swift
+++ Window Shopper/Controller/MainVC.swift
@@ -9,15 +9,25 @@
 import UIKit
 
 class MainVC: UIViewController {
-
+    
+    @IBOutlet weak var hourlyWageTextField: CurrencyTxtField!
+    @IBOutlet weak var priceTextField: CurrencyTxtField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let calcButton = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcButton.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 0.9023169949)
+        calcButton.setTitle("Calculate", for: .normal)
+        calcButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcButton.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        hourlyWageTextField.inputAccessoryView = calcButton
+        priceTextField.inputAccessoryView = calcButton
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func calculate() {
+        print("We made it!")
     }
 
 
